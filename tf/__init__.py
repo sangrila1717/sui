d82a3db9979adac043b4db774033dd4bedede16b
l-tang <litang1025@gmail.com>
2022-04-20 18:28:12 +0800

    feat: import PNN.

---
+++ tf/__init__.py
@@ -0,0 +1,7 @@
+"""sui.tf
+Deep learning algorithm implementations based on TensorFlow.
+"""
+from __future__ import absolute_import, division, print_function, unicode_literals
+from .pnn import PNN
+
+__all__ = ['PNN']
